9ba92a1d19de79d98a734b23a2aafaa973c59f0f
EduardoRFS <theeduardorfs@gmail.com>
2020-04-11 22:23:18 -0300

    Code: Fix compatibility with android

--- runtime/unix.c
+++ runtime/unix.c
@@ -361,7 +361,7 @@ CAMLexport int caml_read_directory(char * dirname, struct ext_table * contents)
 
 char * caml_executable_name(void)
 {
-#if defined(__linux__)
+#if defined(__linux__) && !defined(__ANDROID__)
   int namelen, retcode;
   char * name;
   struct stat st;
